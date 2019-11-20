dd3037cd5a75154a03bc2c91838dc009b7e0d09a
Matt Neely <maneely@microsoft.com>
2019-11-20 09:57:53 -0800

    Change how public keys are referenced.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("086bb804-3b8d-47a3-96d2-b46c2069d2a3")]
 
-[assembly: InternalsVisibleTo("BuildUpToDateChecker.Tests" + StrongNamePublicKeys.CloudBuildPublicKey)]
-[assembly: InternalsVisibleTo("ToDynamicProxyGenAssembly2" + StrongNamePublicKeys.MoqPublicKey)]
+[assembly: InternalsVisibleTo("BuildUpToDateChecker.Tests")]
+[assembly: InternalsVisibleTo("ToDynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
